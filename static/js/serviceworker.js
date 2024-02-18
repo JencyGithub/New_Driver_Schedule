31dcffd9a73a6b2436b2f9fe974d1026cf7ed8ef
kishan <kishanparkhiya167@gmail.com>
2024-02-18 16:49:07 +1100

    Add final PWA from production

--- static/js/serviceworker.js
+++ static/js/serviceworker.js
@@ -10,17 +10,17 @@ event.waitUntil(
 );
 });
 
-self.addEventListener('fetch', function(event) {
-var requestUrl = new URL(event.request.url);
-	if (requestUrl.origin === location.origin) {
-	if ((requestUrl.pathname === '/')) {
-		event.respondWith(caches.match(''));
-		return;
-	}
-	}
-	event.respondWith(
-	caches.match(event.request).then(function(response) {
-		return response || fetch(event.request);
-	})
-	);
-});
+// self.addEventListener('fetch', function(event) {
+// var requestUrl = new URL(event.request.url);
+// 	if (requestUrl.origin === location.origin) {
+// 	if ((requestUrl.pathname === '/')) {
+// 		event.respondWith(caches.match(''));
+// 		return;
+// 	}
+// 	}
+// 	event.respondWith(
+// 	caches.match(event.request).then(function(response) {
+// 		return response || fetch(event.request);
+// 	})
+// 	);
+// });
